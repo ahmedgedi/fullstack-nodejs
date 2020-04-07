e00b94db4f56069da64d00d92efd66b1770b9ed5
Ahmed Gedi <ahmed.gedi@nike.com>
2020-04-06 19:13:50 -0700

    repo rename check

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send({y:'mx + b'})
+    res.send({sounds:'like a lotta hoopla'})
 });
 
 const PORT = process.env.PORT || 5000
