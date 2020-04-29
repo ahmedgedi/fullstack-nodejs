d14c18c55120c2fb7320ea6948cdc4cf76d91b97
Ahmed Gedi <ahmed.gedi1@gmail.com>
2020-04-28 23:12:47 -0700

    added logout feature

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,6 +9,12 @@ module.exports = (app) => {
 
     app.get('/auth/google/callback', passport.authenticate('google'))
 
+    app.get('/api/logout', (req, res) => {
+        // passport attaches a couple of other functions to the req object
+        req.logout(); // this kills the user cookie and logs out the user
+        res.send(req.user)
+    })
+
     app.get('/api/current_user', (req, res) => {
         res.send(req.user);
     })
