164874cc8d70f7c5130e4458f541d731b482270d
Ahmed Gedi <ahmed.gedi1@gmail.com>
2020-05-17 22:18:32 -0700

    heroku fix

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ app.use(passport.initialize())
 app.use(passport.session())
 
 require('./routes/authRoutes')(app)
-
+app.get('/', (req, res) => {
+    res.send({"hello":"world"})
+})
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
