f5f61152237ad84137f416c049aa98e7b1f9668c
Ahmed Gedi <ahmed.gedi1@gmail.com>
2020-06-28 23:53:05 -0700

    header accessing redux store

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-export default class Header extends Component {
+class Header extends Component {
+    renderContent() {
+        switch(this.props.auth) {
+            case null:
+                return 'still deciding...'
+            case false:
+                return <li><a href="/auth/google"> Login with Google </a></li>
+            default:
+                return <li><a href="/api/logout"> Logout </a></li>
+        }
+    }
+    
+    
     render() {
         return (
             <nav>
                 <div className="nav-wrapper">
                 <a href="#" className="left brand-logo"> Emaily </a>
                 <ul id="nav-mobile" className="right hide-on-sm-and-down">
-                    <li><a>Login with Google</a></li>
+                    {this.renderContent()}
                 </ul>
                 </div>
             </nav>
         )
     }
 }
+
+function mapStateToProps({ auth }) {
+    return { auth }
+}
+export default connect(mapStateToProps)(Header)
